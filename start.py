486433b883288d833ebaf5ca22c66b13410e9671
Shashi Mazumdar <shashi@streamsets.com>
2020-02-20 22:16:30 +0000

    TEST-947: Install Transformer on a secure MapR cluster.
    Add support for installing Transformer on a secure MapR cluster.
    
    Change-Id: I80730962a95680e41799ed1097a51aab92dc378d
    Reviewed-on: https://review.streamsets.net/c/topology_mapr/+/29846
    Reviewed-by: Shashi Mazumdar <shashi@streamsets.com>

--- start.py
+++ start.py
@@ -31,6 +31,7 @@ EARLIEST_MAPR_VERSION_WITH_LICENSE_AND_CENTOS_7 = (6, 0, 0)
 EARLIEST_MEP_VERSION_FOR_SETUP_MAPR_SCRIPT = (4, 0)
 MAPR_CONFIG_DIR = '/opt/mapr/conf'
 MAPR_SERVERTICKET_FILE = 'maprserverticket'
+MAPR_USERTICKET_FILE = 'mapruserticket'
 MCS_SERVER_PORT = 8443
 SECURE_CONFIG_CONTAINER_DIR = '/etc/clusterdock/secure'
 SSL_KEYSTORE_FILE = 'ssl_keystore'
@@ -87,13 +88,19 @@ def main(args):
                         devices=node_disks.get(args.primary_node[0]),
                         # Secure cluster needs the ticket to execute rest of commands
                         # after cluster start.
-                        environment=['MAPR_TICKETFILE_LOCATION=/opt/mapr/conf/mapruserticket']
-                        if args.secure else [])
+                        environment=({'MAPR_TICKETFILE_LOCATION': os.path.join(MAPR_CONFIG_DIR,
+                                                                               MAPR_USERTICKET_FILE)}
+                                     if args.secure else {}))
 
     secondary_nodes = [Node(hostname=hostname,
                             group='secondary',
                             image=secondary_node_image,
-                            devices=node_disks.get(hostname))
+                            devices=node_disks.get(hostname),
+                            # Secure cluster needs the ticket to execute rest of commands
+                            # after cluster start.
+                            environment=({'MAPR_TICKETFILE_LOCATION': os.path.join(MAPR_CONFIG_DIR,
+                                                                                   MAPR_USERTICKET_FILE)}
+                                         if args.secure else {}))
                        for hostname in args.secondary_nodes]
 
     # If transformer is specified, add it to the cluster.
@@ -194,14 +201,44 @@ def main(args):
                        time_between_checks=3, timeout=180, success=success, failure=failure)
     mcs_server_host_port = primary_node.host_ports.get(MCS_SERVER_PORT)
 
+    # If this is a secure cluster, then generate a ticket for the mapr user on each node of the
+    # cluster.
+    if args.secure:
+        for node in cluster.nodes:
+            node.execute('maprlogin password <<< "mapr"', user='mapr', quiet=quiet)
+
+        # Restart the hiveserver once a new ticket has been created.  HiveServer2 is configured only on the primary
+        # node.  When the hiveserver starts initially, the metastore schema creation fails because it is missing the
+        # user ticket.  This user ticket is created when the previous command is run.
+        hive_version_str = primary_node.execute('basename `echo /opt/mapr/hive/hive-*`',
+                                                quiet=True).output.strip().split('-')[1]
+        hive_version_home = '/opt/mapr/hive/hive-{}'.format(hive_version_str)
+        primary_node.execute('maprcli node services -name hs2 -action stop -nodes {}'.format(primary_node.fqdn),
+                             user='mapr', quiet=quiet)
+
+        def hiveserver_stopped_condition(node):
+            status = int(node.execute(
+                "maprcli service list -output terse -node {0} | grep hs2 | awk '{{print $1}}'".format(node.fqdn),
+                quiet=quiet).output.strip() or -1)
+            # check if service status is either stopped (3) or not configured (0).
+            return status in (0, 3)
+
+        wait_for_condition(condition=hiveserver_stopped_condition, condition_args=[primary_node], timeout=30)
+
+        primary_node.execute('rm -rf {}/bin/metastore_db'.format(hive_version_home), user='mapr', quiet=quiet)
+        primary_node.execute('cd {}/bin/ && ./schematool -dbType derby -initSchema'.format(hive_version_home), user='mapr',
+                             quiet=quiet)
+        primary_node.execute('maprcli node services -name hs2 -action start -nodes {}'.format(primary_node.fqdn),
+                             user='mapr', quiet=quiet)
+
     logger.info('Creating /apps/spark directory on %s ...', primary_node.hostname)
     spark_directory_command = ['hadoop fs -mkdir -p /apps/spark',
                                'hadoop fs -chmod 777 /apps/spark']
-    primary_node.execute("bash -c '{}'".format('; '.join(spark_directory_command)), quiet=quiet)
+    primary_node.execute("bash -c '{}'".format('; '.join(spark_directory_command)), user='mapr', quiet=quiet)
 
     logger.info('Creating MapR sample Stream named /sample-stream on %s ...', primary_node.hostname)
     primary_node.execute('maprcli stream create -path /sample-stream '
-                         '-produceperm p -consumeperm p -topicperm p', quiet=quiet)
+                         '-produceperm p -consumeperm p -topicperm p', user='mapr', quiet=quiet)
 
     # Determine the spark version
     sparkversion_cmd = primary_node.execute('basename `echo /opt/mapr/spark/spark-*`', quiet=True)
@@ -219,6 +256,29 @@ def main(args):
                                        'spark.executor.instances': '1',
                                        'spark.shuffle.service.enabled': 'true'}
 
+    # Configure core-site.xml so that impersonation works properly.
+    core_site_config_file = '{}/etc/hadoop/core-site.xml'.format(hadoop_version_home)
+    core_site_config_string = ('<property>\n'
+                               '    <name>hadoop.proxyuser.{transformer_user}.hosts</name>\n'
+                               '    <value>*</value>\n'
+                               '</property>\n'
+                               '<property>\n'
+                               '    <name>hadoop.proxyuser.{transformer_user}.groups</name>\n'
+                               '    <value>*</value>\n'
+                               '</property>\n'
+                               '<property>\n'
+                               '    <name>hadoop.proxyuser.{sdc_user}.hosts</name>\n'
+                               '    <value>*</value>\n'
+                               '</property>\n'
+                               '<property>\n'
+                               '    <name>hadoop.proxyuser.{sdc_user}.groups</name>\n'
+                               '    <value>*</value>\n'
+                               '</property>\n'
+                               '<property>\n'
+                               '    <name>fs.mapr.server.resolve.user</name>\n'
+                               '    <value>true</value>\n'
+                               '</property>\n'.format(transformer_user=st.ST_USER, sdc_user='sdc'))
+
     # Configure the yarn-site.xml file for external shuffle service
     yarn_site_config_file = '{}/etc/hadoop/yarn-site.xml'.format(hadoop_version_home)
     yarn_config_string = ('<property>\n'
@@ -229,6 +289,12 @@ def main(args):
                           '    <name>yarn.nodemanager.aux-services.spark_shuffle.class</name>\n'
                           '    <value>org.apache.spark.network.yarn.YarnShuffleService</value>\n'
                           '</property>\n')
+    if args.secure:
+        yarn_config_string += ('<property>\n'
+                               '    <name>spark.authenticate</name>\n'
+                               '    <value>true</value>\n'
+                               '</property>\n'
+                              )
 
     # The location of the shuffle jar depends on the spark version.
     spark_shuffle_jar = 'yarn/spark-*-mapr-*-yarn-shuffle.jar'
@@ -246,6 +312,16 @@ def main(args):
         spark_default_config.update(spark_dynamic_allocation_config)
         node.put_file(spark_defaults_config_file, PropertiesFile.dumps(spark_default_config))
 
+        # Update the core-site.xml config on each node
+        core_site_config = node.get_file(core_site_config_file)
+        node.put_file(core_site_config_file,
+                      re.sub('(</configuration>)', '{}\\1'.format(core_site_config_string), core_site_config))
+
+        # On each node, create files in the /opt/mapr/conf/proxy directory to support
+        # impersonation.  For impersonation on MapR, the transfomer user or the sdc user can impersonate.
+        node.execute('touch /opt/mapr/conf/proxy/{}'.format(st.ST_USER), quiet=quiet)
+        node.execute('touch /opt/mapr/conf/proxy/{}'.format('sdc'), quiet=quiet)
+
         # Update the yarn-site.xml config on each node
         yarn_site_config = node.get_file(yarn_site_config_file)
         node.put_file(yarn_site_config_file,
@@ -253,9 +329,29 @@ def main(args):
 
         node.execute(spark_config_commands, quiet=quiet)
 
-    # Restart services on all nodes.  This is needed so that the config changes made above can take affect.
+    # Set the cldb.security.resolve.user property to 1.  This is necessary for impersonation to work properly
+    primary_node.execute('maprcli config save -values {cldb.security.resolve.user:1}', quiet=quiet)
+
+    # We need to wait for the cldb service to start before proceeding. the cldb service is configured only on the
+    # primary node.
+    primary_node.execute('maprcli node services -name cldb -action restart -nodes {}'.format(primary_node.fqdn),
+                         quiet=quiet)
+
+    def cldb_started_condition(node):
+        cldb_status = int(node.execute(
+            "maprcli service list -output terse -node {0} | grep cldb | awk '{{print $1}}'".format(node.fqdn),
+            quiet=quiet).output.strip() or -1)
+        # Check to see if the service status is either 0 (not configured on the node) or 2 (running).
+        return cldb_status in (0, 2)
+
+    wait_for_condition(condition=cldb_started_condition, condition_args=[primary_node], timeout=120)
+
+    # Restart services whose configuration was modified on the nodes.  This is needed so that the config changes made
+    # above can take affect. If the service is not configured on the node it is a no-op.
     nodes = ' '.join([node.fqdn for node in cluster.nodes])
     primary_node.execute('maprcli node services -name nodemanager -action restart -nodes {}'.format(nodes), quiet=quiet)
+    primary_node.execute('maprcli node services -name resourcemanager -action restart -nodes {}'.format(nodes),
+                         quiet=quiet)
 
     if mapr_version_tuple >= EARLIEST_MAPR_VERSION_WITH_LICENSE_AND_CENTOS_7 and args.license_url:
         license_commands = ['curl --user {} {} > /tmp/lic'.format(args.license_credentials,
@@ -275,9 +371,9 @@ def main(args):
         primary_node.execute(' && '.join(register_gateway_commands), quiet=quiet)
 
     logger.info('Creating sdc user directory in MapR-FS ...')
-    create_sdc_user_directory_command = ['sudo -u mapr hadoop fs -mkdir -p /user/sdc',
-                                         'sudo -u mapr hadoop fs -chown sdc:sdc /user/sdc']
-    primary_node.execute('; '.join(create_sdc_user_directory_command), quiet=quiet)
+    create_sdc_user_directory_command = ['hadoop fs -mkdir -p /user/sdc',
+                                         'hadoop fs -chown sdc:sdc /user/sdc']
+    primary_node.execute('; '.join(create_sdc_user_directory_command), user='mapr', quiet=quiet)
 
     if args.sdc_version:
         logger.info('Installing StreamSets DataCollector version %s ...', args.sdc_version)
@@ -299,10 +395,9 @@ def main(args):
             primary_node.execute('chown -R {user}:{user} {path}'.format(user=st.ST_USER, path=path))
 
         logger.info('Creating transformer user directory in MapR-FS ...')
-        create_transformer_user_directory_command = ['sudo -u mapr hadoop fs -mkdir -p /user/{}'.format(st.ST_USER),
-                                                     'sudo -u mapr hadoop fs -chown {0}:{0} /user/{0}'.format(
-                                                         st.ST_USER)]
-        primary_node.execute('; '.join(create_transformer_user_directory_command), quiet=quiet)
+        create_transformer_user_directory_command = ['hadoop fs -mkdir -p /user/{}'.format(st.ST_USER),
+                                                     'hadoop fs -chown {0}:{0} /user/{0}'.format(st.ST_USER)]
+        primary_node.execute('; '.join(create_transformer_user_directory_command), user='mapr', quiet=quiet)
 
         if args.sch_server_url:
             logger.info('Enabling StremSets Control Hub for Transformer ...')
@@ -326,6 +421,16 @@ def main(args):
 
         logger.info('Running Transformer as user %s from %s ...', st.ST_USER, transformer.home_dir)
         command_for_execute = transformer.command_for_execute()
+        if args.secure:
+            # If this is a secure MapR cluster, create a service ticket for use with the transformer process
+            transformer_service_ticket_location = '{}/maprservice_ticket'.format(transformer.home_dir)
+            primary_node.execute('maprlogin generateticket -type servicewithimpersonation -out {} -duration 30:0:0 '
+                                 '-renewal 90:0:0 -user {}'.format(transformer_service_ticket_location, st.ST_USER),
+                                 quiet=quiet)
+            primary_node.execute('chown {0}:{0} {1}'.format(st.ST_USER, transformer_service_ticket_location),
+                                 quiet=quiet)
+            command_for_execute = 'export MAPR_TICKETFILE_LOCATION={} && {}'.format(transformer_service_ticket_location,
+                                                                                    command_for_execute)
         primary_node.execute(command_for_execute, user=st.ST_USER, detach=True, quiet=quiet)
 
         def st_condition(command):
